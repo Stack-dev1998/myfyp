ad3237c3b8b98f6f2c24694e1f36c3067e0a2de5
Hassan Khan <hassankust1@gmail.com>
2021-03-03 12:29:09 +0500

    Updates

---
+++ precache-manifest.ab8dfe0ee9d36ebe382780f9032bc24e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29330d5fdea3d66a3da5af2ab9b1dde0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ea0556faa5639d42c315",
+    "url": "/static/css/2.ff645aaf.chunk.css"
+  },
+  {
+    "revision": "9eba94306d706bfc047f",
+    "url": "/static/css/main.a0fdb893.chunk.css"
+  },
+  {
+    "revision": "ea0556faa5639d42c315",
+    "url": "/static/js/2.afebc1e1.chunk.js"
+  },
+  {
+    "revision": "558d6e79284a423931154cc5423ba1dc",
+    "url": "/static/js/2.afebc1e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9eba94306d706bfc047f",
+    "url": "/static/js/main.e1626f8e.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  },
+  {
+    "revision": "76ca7124634aeed7e885feae36ed2466",
+    "url": "/static/media/2.76ca7124.jpg"
+  },
+  {
+    "revision": "8a35b46f7e74e5e4d0ce498aa4edff40",
+    "url": "/static/media/poor_child1.8a35b46f.jpg"
+  },
+  {
+    "revision": "1c0169d15445fbb276aca5d868b1cd54",
+    "url": "/static/media/up-link.1c0169d1.svg"
+  }
+]);
